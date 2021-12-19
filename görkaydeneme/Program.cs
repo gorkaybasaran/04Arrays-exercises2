2bb8f846df1980b2e4b3164bf47d40f14c670a03
gorka <gorka@DESKTOP-902BBMO>
2021-12-19 03:10:21 +0100

    Proje dosyası ekle.

---
+++ görkaydeneme/Program.cs
@@ -0,0 +1,30 @@
+﻿
+	
+		int n, i, p, k;
+
+		Console.Write("Enter a number:");
+		n = Convert.ToInt32(Console.ReadLine());
+
+		for (i = 2; i <= n; i++)
+		{
+			k = 2;
+			p = 1;
+			while (k < i)
+			{
+				if (i % k == 0)
+				{
+					p = 0;
+					break;
+				}
+				k++;
+			}
+			if (p == 1)
+			{
+				Console.WriteLine(+ i);
+			}
+		}
+
+	
+
+
+
